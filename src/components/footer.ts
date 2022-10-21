e319b759e1025737ad7e418e65a011b2d8522829
Alina <alilaursdesign@gmail.com>
2022-10-21 19:31:26 +0200

    Add all files

---
+++ src/components/footer.ts
@@ -0,0 +1,19 @@
+import { Component } from './component.js';
+import styles from './footer.module.css' assert { type: 'css' };
+
+export class Footer extends Component {
+    template: string;
+    constructor(public selector: string) {
+        super();
+        this.template = this.createTemplate();
+        this.renderAdd(this.selector, this.template);
+        console.log(styles);
+    }
+    createTemplate() {
+        return `
+         <footer class="${styles.footer}"
+            <address>ISDI COders</address>
+        </footer>
+        `;
+    }
+}
